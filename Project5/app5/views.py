fbe1ce1d4126cf0a0d2404eec27dce39440156a4
Satinder Sharma <47383766+satindersharma@users.noreply.github.com>
2021-09-11 11:33:58 +0530

    project 5

---
+++ Project5/app5/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Employee
+# Create your views here.
+def home(request):
+    # emp = Employee.objects.filter(age=32)
+    emp = Employee.objects.all()
+    print(emp)
+    context={'emp':emp}
+    return render(request,'home.html',context)
+
+
+
+def showemployee(request,pk):
+    single_emp = Employee.objects.get(pk=pk)
+    # print(single_emp)
+
+    context = {'single_emp':single_emp}
+    # context = {}
+    return render(request,'employee.html',context)
